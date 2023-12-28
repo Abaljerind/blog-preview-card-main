a072f710c6271a09479325664bc1177ef45f64d0
Abaljerind <abaljerinda@gmail.com>
2023-12-28 17:34:10 +0700

    add style to every elements HTML

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,11 +1,13 @@
 export default function Article() {
   return (
     <>
-      <p>Learning</p>
-      <p>Published 21 Dec 2023</p>
+      <p className="text-black bg-yellow py-1 px-3 mt-5 inline-block font-semibold rounded-md text-base">
+        Learning
+      </p>
+      <p className="text-base font-semibold mt-3 mb-3">Published 21 Dec 2023</p>
 
-      <h2>HTML & CSS foundations</h2>
-      <p>
+      <h2 className="text-xl font-extrabold mb-3">HTML & CSS foundations</h2>
+      <p className="text-gray-400 text-base">
         These languages are the backbone of every website, defining structure,
         content, and presentation.
       </p>
