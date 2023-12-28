3700268e1678da1af033f6460f40f12615d49f33
Abaljerind <abaljerinda@gmail.com>
2023-12-28 15:35:42 +0700

    create Main.jsx & add img with tailwind style

---
+++ src/Components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main() {
+  return (
+    <>
+      <main className="flex justify-center items-center h-[720px]">
+        <section className="bg-white inline-block p-4 border-black border-[1px] rounded-lg">
+          <img
+            className="rounded-lg"
+            src={require("../illustration-article.svg").default}
+            alt="illustration article"
+          />
+        </section>
+      </main>
+    </>
+  );
+}
