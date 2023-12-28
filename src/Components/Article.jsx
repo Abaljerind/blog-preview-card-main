d314984a68f96fe81382023cb4ed530b1c480932
Abaljerind <abaljerinda@gmail.com>
2023-12-28 18:01:37 +0700

    add style hover to h2 & finish

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -8,7 +8,9 @@ export default function Article() {
       </p>
       <p className="text-base font-semibold mt-3 mb-3">Published 21 Dec 2023</p>
 
-      <h2 className="text-xl font-extrabold mb-3">HTML & CSS foundations</h2>
+      <h2 className="text-xl font-extrabold mb-3 hover:text-yellow hover:cursor-pointer hover:duration-500">
+        HTML & CSS foundations
+      </h2>
       <p className="text-gray-400 text-base">
         These languages are the backbone of every website, defining structure,
         content, and presentation.
