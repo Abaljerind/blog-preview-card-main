a319ef480b4e0e368df2e3999c9ecc7892a00fe0
Abaljerind <abaljerinda@gmail.com>
2023-12-28 17:27:51 +0700

    add contents

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,3 +1,14 @@
 export default function Article() {
-  return <></>;
+  return (
+    <>
+      <p>Learning</p>
+      <p>Published 21 Dec 2023</p>
+
+      <h2>HTML & CSS foundations</h2>
+      <p>
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+      </p>
+    </>
+  );
 }
