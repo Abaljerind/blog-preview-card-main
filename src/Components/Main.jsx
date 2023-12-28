fcc5aa67c020293cbf7f027e03f3fa50aeb0a40d
Abaljerind <abaljerinda@gmail.com>
2023-12-28 17:59:32 +0700

    add box shadow style

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <>
       <main className="flex justify-center items-center h-[720px]">
-        <section className="w-[360px] bg-white inline-block p-4 border-black border-2 rounded-2xl">
+        <section className="w-[360px] bg-white inline-block p-4 border-black border-2 rounded-2xl shadow-solid">
           <img
             className="rounded-lg"
             src={require("../illustration-article.svg").default}
