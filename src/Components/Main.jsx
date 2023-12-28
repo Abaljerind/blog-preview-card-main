cca437e6995791911c46c44c2c0cf63bc266c808
Abaljerind <abaljerinda@gmail.com>
2023-12-28 17:22:40 +0700

    add style width, padding, border

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <>
       <main className="flex justify-center items-center h-[720px]">
-        <section className="bg-white inline-block p-4 border-black border-[1px] rounded-lg">
+        <section className="w-[360px] bg-white inline-block p-4 border-black border-2 rounded-2xl">
           <img
             className="rounded-lg"
             src={require("../illustration-article.svg").default}
