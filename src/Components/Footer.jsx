1e4ae0553db81643ff68c5800e54fcd8e52cc95a
Abaljerind <abaljerinda@gmail.com>
2023-12-28 17:55:13 +0700

    add img & p element inside footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,1 +1,14 @@
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <footer className="mt-3 relative">
+      <img
+        className="w-10"
+        src={require("../image-avatar.webp")}
+        alt="Greg Hooper"
+      />
+      <p className="absolute top-2 left-12 font-extrabold text-base">
+        Greg Hooper
+      </p>
+    </footer>
+  );
+}
